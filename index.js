f2e17d698919600faee97513ebd7e93af3448020
Ujjaval-99 <ujjavalmistry99@gmail.com>
2023-05-08 17:38:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function multiplyBy()
+{
+  num1 = document.getElementById(
+    "firstNumber").value;
+  num2 = document.getElementById(
+    "secondNumber").value;
+  document.getElementById(
+    "result").innerHTML = num1 * num2;
+}
